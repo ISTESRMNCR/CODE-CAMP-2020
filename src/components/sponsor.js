2b10a8a4236e3ca2f644027deda04a019c82ed91
Rikesh Makwana <rikesh.makwana@gmail.com>
2020-07-03 14:16:06 +0400

    Fix typo and image fit for sponsors section

--- src/components/sponsor.js
+++ src/components/sponsor.js
@@ -13,31 +13,31 @@ return (
   <section style={{ padding:10, textAlign:"center",backgroundColor:"#fff", }}>
 
     <div className="container" style={{ padding: 30, paddingTop:0}}>
-  <div className="imgage-container" style={{ textAlign:"center", padding: 10 }}> 
+  <div className="image-container" style={{ textAlign:"center", padding: 10 }}> 
   <p style={{ textAlign:"center", fontSize:30, fontWeight:700 }} >OUR SPONSORS 😍 </p>
     <a href="https://balsamiq.cloud/" target="_blank" >
-      <img src={balsamiq} className="imgage"/>
+      <img src={balsamiq} className="image"/>
     </a>
     <a href="https://dare2compete.com/" target="_blank" >
-      <img src="https://d8it4huxumps7.cloudfront.net/images/d2c-logo/d2clogo.svg" alt="Dare2Compete" className="imgage"/>
+      <img src="https://d8it4huxumps7.cloudfront.net/images/d2c-logo/d2clogo.svg" alt="Dare2Compete" className="image"/>
     </a>
     <a href="https://www.wolfram.com/hackathons/" target="_blank">
-      <img src={wolfram} alt="wolfram" className="imgage"/>
+      <img src={wolfram} alt="wolfram" className="image"/>
     </a>
     <a href="https://rosenfeldmedia.com/" target="_blank" >
-      <img src={rosenfeld} alt="rosenfeld" className="imgage"/>
+      <img src={rosenfeld} alt="rosenfeld" className="image"/>
     </a>
     <a href="https://repl.it//" target="_blank" >
-      <img src={replit} alt="replit" className="imgage"/>
+      <img src={replit} alt="replit" className="image"/>
     </a>
     <a href="https://1password.com/?gclid=Cj0KCQjw6PD3BRDPARIsAN8pHuGFKTd6Lb755fCq_YegAYQ6UiApCo3QpboyOvRroGuNFk9BLnnCRmEaAjAsEALw_wcB&gclsrc=aw.ds" target="_blank">
-      <img src={password} alt="password" className="imgage"/>
+      <img src={password} alt="password" className="image"/>
     </a>
     <a href="https://edtimes.in/" target="_blank" >
-      <img src={edtimes} alt="edtimes" className="imgage"/>
+      <img src={edtimes} alt="edtimes" className="image"/>
     </a>
     <a href="https://proto.io/" target="_blank" >
-      <img src={proto} alt="proto" className="imgage"/>
+      <img src={proto} alt="proto" className="image"/>
     </a>
 
   </div>
