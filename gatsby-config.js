6cd7b018455a8c68324a00d9448e0dae63deaee0
Utkarsh Yadav <yadavutkarsh207@gmail.com>
2020-07-10 00:50:53 +0530

    Google: Tracker Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,7 +88,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-172226142-1`,
       },
     },
     `gatsby-plugin-feed`,
@@ -111,8 +111,5 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
